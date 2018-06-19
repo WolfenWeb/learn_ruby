2f8f628e53a314ff9d704b389603fa7c46e22c41
Patrick Swannick <pswannic326@gmail.com>
2018-06-19 00:38:25 -0400

    Initial Commit - Working

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
+# Book Class from Odin Project
 class Book
-# write your code here
+  attr_reader :title
+  def initialize() end
+
+  def title=(param1)
+    conjuctions = %w[and or a an in the of]
+    @title = param1.split.map.with_index do |word, index|
+      if index.zero? || !conjuctions.include?(word)
+        word.capitalize
+      else
+        word
+      end
+    end.join(' ')
+  end
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,16 @@
+# Timer stores seconds, echoes HH:MM:SS
 class Timer
-  #write your code here
+  attr_accessor :seconds
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    time_string = []
+    time_string[0] = @seconds / 3600
+    time_string[1] = @seconds % 3600 / 60
+    time_string[2] = @seconds % 3600 % 60
+    time_string.map!{ |i| i.to_s.rjust(2, '0')   }
+    time_string.join(':')
+  end
 end
