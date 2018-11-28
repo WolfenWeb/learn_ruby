62a999e91ec62f857066b526b419f63c7e377c92
WolfenWeb <39467520+WolfenWeb@users.noreply.github.com>
2018-11-28 09:34:19 -0500

    Test commit

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -6,7 +6,7 @@ class Timer
   end
 
   def time_string
-    time_string = []
+    time_string =  []
     time_string[0] = @seconds / 3600
     time_string[1] = @seconds % 3600 / 60
     time_string[2] = @seconds % 3600 % 60
